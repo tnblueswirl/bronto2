b51f5a38a42bde7f5595238243c47538a613cdfe
Adam Daniels <tnblueswirl@gmail.com>
2013-10-31 14:04:32 -0400

    starting config settings

---
+++ app/code/Bronto/Common/Helper/Data.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Bronto Common Helper
+ *
+ * @category    Bronto2
+ * @package     Bronto_Common
+ * @author      Adam Daniels <adam.daniels@atlanticbt.com>
+ */
+namespace Bronto\Common\Helper;
+
+class Data extends \Magento\Core\Helper\Data
+{
+
+}
